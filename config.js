0a73753007e7bda74cbf426049614f12908be4cf
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:09:13 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ module.exports = {
   info55: 'Tlenoterapia hiperbaryczna wspiera proces aktualnie prowadzonych terapii i rehabilitacji. Przyśpiesz oczekiwany rezultat zdrowia, aby Twój organizm funkcjonował wydajniej!',
   //zastosowania
   zastosowania1: 'Kardiologia',
-  zastosowania11: ''
+  zastosowania11: 'Korzystny wpływ na dolegliwości o charakterze wieńcowym. Zmniejsza nasilenie objawów niewydolności serca, zapobiega zawałom mięśnia sercowego, reguluje ciśnienie tętnicze.'
   // social
   socialLinks: [
     {
