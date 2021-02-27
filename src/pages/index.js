9af28824c4079d9103e804de426977738cecc161
Stackbit Code Editor <support@stackbit.com>
2021-02-27 21:07:29 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -149,7 +149,7 @@ const IndexPage = () => (
               {config.zastosowania21}
             </p>
           </li>
-          <li className="icon solid fa-diamo">
+          <li className="icon solid fa-diamond">
             <h3>{config.zastosowania3}</h3>
             <p>
               {config.zastosowania31}
