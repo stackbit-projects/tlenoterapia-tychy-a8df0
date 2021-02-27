6b3d3ddabfe92a44f9ef808b8613fd5a75e543e7
Stackbit Code Editor <support@stackbit.com>
2021-02-27 21:22:09 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,12 +15,12 @@ export default function Footer() {
           );
         })}
       </ul>
-      <ul className="copyright">
+      {/* <ul className="copyright">
         <li>&copy; Spectral</li>
         <li>
           Design: <a href="http://html5up.net">HTML5 UP</a>
         </li>
-      </ul>
+      </ul> */}
     </footer>
   );
 }
