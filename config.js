353879c3088bb4bee567e9b76b08e3434f12226e
Stackbit Code Editor <support@stackbit.com>
2021-02-27 19:07:22 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   info2: 'Zyskaj wsparcie w trakcie aktualnej rehabilitacji',
   info22: 'Tlenoterapia hiperbaryczna wspiera proces aktualnie prowadzonych terapii i rehabilitacji. Przyśpiesz oczekiwany rezultat zdrowia, aby Twój organizm funkcjonował wydajniej!',
   info3: 'W odpowiedzi na pytania, które zadaje zdrowie!',
-  info
+  info33: ''
     // social
   socialLinks: [
     {
