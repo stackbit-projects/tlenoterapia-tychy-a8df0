ba34e5c79791493d026f26840540e0d81840750e
Stackbit Code Editor <support@stackbit.com>
2021-02-27 19:48:33 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ const IndexPage = () => (
             </span>
           </li>
           <li>
-            <span className="icon solid fa-heart major style3">
+            <span className="icon solid fa-heartbeat major style3">
               <span className="label">tlen</span>
             </span>
           </li>
