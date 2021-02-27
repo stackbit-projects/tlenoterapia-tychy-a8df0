5e11eaa3fe42c790996fc24aa58f237816ba7e20
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:14:45 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -43,7 +43,8 @@ module.exports = {
   zastosowania55: 'Pomaga w leczeniu ran po zabiegach chirurgicznych, poprawia ukrwienie w uszkodzonej tkance, stymuluje proces gojenia oraz skraca czas rekonwalescencji.',
   zastosowania6: 'Medycyna sportowa',
   zastosowania66: 'Podnosi wydolność fizyczną, przyspiesza procesy regeneracji tkanki mięśniowej, ścięgien i więzadeł. Działa przeciwbólowo, wpływa na procesy naprawcze w tkance kostnej.',
-  zastosowania7: ''
+  zastosowania7: 'Medycyna estetyczna',
+  zas
 
   // social
   socialLinks: [
