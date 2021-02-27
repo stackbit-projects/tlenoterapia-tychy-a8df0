709d61ac68e5bee115402941fd01353de7930d1f
Stackbit Code Editor <support@stackbit.com>
2021-02-27 19:05:15 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ module.exports = {
   tekst1: 'Nasza komora hiperbaryczna wykorzystuje ciśnienie 1,3 ATA. Jest to nieiwazyjna metoda wprowadzania tlenu o stężeniu 93%, czyli o 30% wyższym niż ciśnienie atmosferyczne.',
   info1: 'Wzmocnij odporność swojego organizmu!',
   info11: 'Terapia tlenowa ma wszechstronny, pozytywny wpływ na nasze samopoczucie psychiczne i fizyczne. Pozwala zachować na dłużej młodość, dobre zdrowie, aktywuje siły witalne i odpornościowe',
+  info2: 'Zyskaj wsparcie w trakcie aktualnej rehabilitacji'
     // social
   socialLinks: [
     {
