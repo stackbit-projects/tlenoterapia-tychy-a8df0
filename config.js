c288ec1bac52a8fdb57f2b7f4c0e8272a73a2734
Stackbit Code Editor <support@stackbit.com>
2021-02-27 18:56:19 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Oxy-Rehab TlenoterapiaTLENOTERIAPIA TYCHY',
+  heading: 'Oxy-Rehab Tlenoterapia TLENOTERIAPIA TYCHY',
   subHeading: 'Zapraszamy na sesje tlenowe w Tychach ',
   explore: 'Dowiedz się więcej ',
     // social
