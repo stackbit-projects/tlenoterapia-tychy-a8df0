7f16ff2f286e369e8a23d63766aa86a0355625b3
Stackbit Code Editor <support@stackbit.com>
2021-02-27 21:12:23 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -49,7 +49,8 @@ module.exports = {
   zastosowania81: 'Sprzyja rewitalizacji komórek i zapobiega dalszym uszkodzeniom. Oczyszcza organizm z toksyn i metali ciężkich, obniża poziom białka C-reaktywnego.',
   zastosowania9: 'Gastrologia',
   zastosowania91: 'Poprawia ukrwienie ścian jelit i redukuje ilość gazów jelitowych. Wspiera regenerację stanów zapalnych, np. jelita cienkiego, jelita grubego, trzustki. Zabija szkodliwe bakterie i wirusy.',
-
+// kontakt
+kontakt:
 
   // social
   socialLinks: [
