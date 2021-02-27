3a1905b7aef56ceafdbea14a55b40ff3c483423f
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:13:40 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ module.exports = {
   zastosowania4: 'Neurologia',
   zastosowania44: 'Ma zastosowanie w porażeniach mózgowych i pourazowych uszkodzeniach mózgu, powstałych w wyniku udaru lub wylewu. Ożywia i pobudza uśpione komórki, podnosi koncentrację.',
   zastosowania5: 'Stomatologia',
-  zastosowania55: ''
+  zastosowania55: 'Pomaga w leczeniu ran po zabiegach chirurgicznych, poprawia ukrwienie w uszkodzonej tkance, stymuluje proces gojenia oraz skraca czas rekonwalescencji.'
 
   // social
   socialLinks: [
