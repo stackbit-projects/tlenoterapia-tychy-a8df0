f483bc75ec6ce9998620b625e312f51557c314e2
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:21:21 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -170,8 +170,7 @@ const IndexPage = () => (
           <li className="icon fa-flag">
             <h3>{config.zastosowania6}</h3>
             <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
+              {config.zastosowania1}
             </p>
           </li>
         </ul>
