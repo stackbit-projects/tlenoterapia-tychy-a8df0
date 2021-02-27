8324313c5bdb0f412fb1d58f8a87f1403ef1bef3
Stackbit Code Editor <support@stackbit.com>
2021-02-27 18:32:12 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'TLENOTERIAPIA TYCHY',
-  subHeading: 'samia Full time Web Developer. Part time Open source contributor  ',
+  subHeading: 'Zapraszamy na sesje tlenowe  ',
   // social
   socialLinks: [
     {
