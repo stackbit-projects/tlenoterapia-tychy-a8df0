e5de4c524a60dd76ff485e7cc0f3f9fb67570871
Stackbit Code Editor <support@stackbit.com>
2021-02-27 21:27:43 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -201,7 +201,7 @@ const IndexPage = () => (
 				<section>
 					<h4>Skontaktuj się z nami</h4>
                     <p>{config.kontakttel} <br> </br>
-                    {config.kontaktul}
+                   
                     {config.kontaktty}</p>
 					<form method="post" action="#">
 						<div className="row gtr-uniform">
