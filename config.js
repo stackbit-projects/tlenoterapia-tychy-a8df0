732956505415064aec978edf20f7a08d2fad476e
Stackbit Code Editor <support@stackbit.com>
2021-02-27 19:57:57 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -31,6 +31,7 @@ module.exports = {
   info5: 'Komora hiperbaryczna',
   info55: 'Nasza komora hiperbaryczna wykorzystuje ciśnienie 1,3 ATA. Jest to nieiwazyjna metoda wprowadzania tlenu o stężeniu 93%, czyli o 30% wyższym niż ciśnienie atmosferyczne.',
   // social
+  
   socialLinks: [
     {
       style: 'brands',
