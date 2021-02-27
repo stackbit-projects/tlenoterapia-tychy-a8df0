8d656093ce8a745149b676195f3c00ca7c3b09e3
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:18:37 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -144,7 +144,7 @@ const IndexPage = () => (
             </p>
           </li>
           <li className="icon solid fa-laptop">
-            <h3>Ac Augue Eget</h3>
+            <h3>{config.zastosowania1}</h3>
             <p>
               Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
               tincidunt nullam amet leo Aenean ligula consequat consequat.
