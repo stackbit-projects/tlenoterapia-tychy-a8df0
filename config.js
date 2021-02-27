4b4d72bb852221160577f55f3fc8109d8e7729c8
Stackbit Code Editor <support@stackbit.com>
2021-02-27 19:21:49 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -24,7 +24,6 @@ module.exports = {
       'Hiperbaryczna terapia tlenowa to naturalna metoda dbania o urodę.',
       'zwiększenie masy kostnej i masy mięśniowej',
       'wzmocnienie funkcjonowania układu odpornościowego, oddechowego i pokarmowego',
-
 działanie przeciwbólowe i przeciwzapalne
 
 poprawa mobilności ruchowej
