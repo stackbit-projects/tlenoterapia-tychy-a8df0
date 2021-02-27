c788db0c1c73deea46fb93244fbf2676f44a7fb2
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:10:18 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -33,7 +33,9 @@ module.exports = {
   //zastosowania
   zastosowania1: 'Kardiologia',
   zastosowania11: 'Korzystny wpływ na dolegliwości o charakterze wieńcowym. Zmniejsza nasilenie objawów niewydolności serca, zapobiega zawałom mięśnia sercowego, reguluje ciśnienie tętnicze.',
-  zastosowania2: ''
+  zastosowania2: 'Ortopedia',
+  zastosowania22: 'Poprawia przepływ krwi w uszkodzonych kończynach, zwiększa ciśnienie tlenu w kościach powodując ustępowanie stanu zapalnego kości i szpiku.',
+  zastosowania3: 
   // social
   socialLinks: [
     {
