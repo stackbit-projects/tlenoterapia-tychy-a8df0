1be82d9019323cae573f63b8d515fd9282c8f3f9
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:16:49 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ module.exports = {
   zastosowania3: 'Dermatologia',
   zastosowania31: 'Terapia ma zastosowanie w leczeniu trądziku, łuszczycy, atopowego zapalenia skóry, egzemy oraz trądziku różowatego. Redukuje wszelkie ogniska zapalne.',
   zastosowania4: 'Neurologia',
-  zastosowania44: 'Ma zastosowanie w porażeniach mózgowych i pourazowych uszkodzeniach mózgu, powstałych w wyniku udaru lub wylewu. Ożywia i pobudza uśpione komórki, podnosi koncentrację.',
+  zastosowania41: 'Ma zastosowanie w porażeniach mózgowych i pourazowych uszkodzeniach mózgu, powstałych w wyniku udaru lub wylewu. Ożywia i pobudza uśpione komórki, podnosi koncentrację.',
   zastosowania5: 'Stomatologia',
   zastosowania55: 'Pomaga w leczeniu ran po zabiegach chirurgicznych, poprawia ukrwienie w uszkodzonej tkance, stymuluje proces gojenia oraz skraca czas rekonwalescencji.',
   zastosowania6: 'Medycyna sportowa',
