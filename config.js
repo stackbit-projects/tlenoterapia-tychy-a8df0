b02a622b25a178699258aae8e1629e7f336214b2
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:07:06 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -30,6 +30,7 @@ module.exports = {
   ],
   info5: 'Komora hiperbaryczna',
   info55: 'Tlenoterapia hiperbaryczna wspiera proces aktualnie prowadzonych terapii i rehabilitacji. Przyśpiesz oczekiwany rezultat zdrowia, aby Twój organizm funkcjonował wydajniej!',
+  
   // social
   socialLinks: [
     {
