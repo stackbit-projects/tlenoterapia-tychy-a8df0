bd57e7e71fb689523138aae4cf3c04854d7867a7
Stackbit Code Editor <support@stackbit.com>
2021-02-27 20:16:01 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ module.exports = {
   zastosowania77: 'Działa stymulująco na fibroblasty, odpowiedzialne za syntezę kolagenu i elastyny, czyli włókien zapewniających elastyczność skórze.',
   zastosowania8: 'Onkologia',
   zastosowania88: 'Sprzyja rewitalizacji komórek i zapobiega dalszym uszkodzeniom. Oczyszcza organizm z toksyn i metali ciężkich, obniża poziom białka C-reaktywnego.',
-  zas
+  zastosowania9: ''
 
   // social
   socialLinks: [
