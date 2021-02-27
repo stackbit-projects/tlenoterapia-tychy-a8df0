06e4db4e3723a22ba3e9567e56241b29f45512d7
Stackbit Code Editor <support@stackbit.com>
2021-02-27 18:50:12 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
           <li>
             <Scroll type="id" element="one">
               <a href="/#" className="button primary">
-                EXXX
+                EXXXplorrrre
               </a>
             </Scroll>
           </li>
