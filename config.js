7cee95ae00198ed73215b39c58f4c07f0492c7e1
Stackbit Code Editor <support@stackbit.com>
2021-02-27 18:30:49 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'tleoteriapia tychy', // <title>
+  siteTitle: 'tlenoteriapia tychy', // <title>
   manifestName: 'Spectral',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
